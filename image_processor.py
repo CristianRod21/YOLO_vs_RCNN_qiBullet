d1918ed9b6e411b9ccdb3b18a2bdc339cb1bd81a
CristianRod21 <cjrs21@hotmail.com>
2020-05-28 11:57:41 -0600

    YOLO listener process.
    Waits for frames send by the simulation. Process them, and return the object found.

---
+++ image_processor.py
@@ -0,0 +1,168 @@
+import numpy as np
+from multiprocessing import shared_memory
+import socket
+import json
+import cv2
+
+import time
+from absl import app, flags, logging
+from absl.flags import FLAGS
+
+import time
+from absl import app, flags, logging
+from absl.flags import FLAGS
+import core.utils as utils
+from core.yolov4 import YOLOv4, YOLOv3, YOLOv3_tiny, decode
+from PIL import Image
+from core.config import cfg
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+import tensorflow as tf
+import matplotlib.pyplot as pyplot
+flags.DEFINE_string('framework', 'tf', '(tf, tflite')
+flags.DEFINE_string('weights', './data/yolov4.weights',
+                    'path to weights file')
+flags.DEFINE_integer('size', 608, 'resize images to')
+flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
+flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
+flags.DEFINE_string('image', './data/kite.jpg', 'path to input image')
+flags.DEFINE_string('output', 'result.png', 'path to output image')
+
+def main(_argv):
+    if FLAGS.tiny:
+        STRIDES = np.array(cfg.YOLO.STRIDES_TINY)
+        ANCHORS = utils.get_anchors(cfg.YOLO.ANCHORS_TINY, FLAGS.tiny)
+    else:
+        STRIDES = np.array(cfg.YOLO.STRIDES)
+        if FLAGS.model == 'yolov4':
+            ANCHORS = utils.get_anchors(cfg.YOLO.ANCHORS, FLAGS.tiny)
+        else:
+            ANCHORS = utils.get_anchors(cfg.YOLO.ANCHORS_V3, FLAGS.tiny)
+    NUM_CLASS = len(utils.read_class_names(cfg.YOLO.CLASSES))
+    XYSCALE = cfg.YOLO.XYSCALE
+    input_size = FLAGS.size
+    image_path = FLAGS.image
+
+    if FLAGS.framework == 'tf':
+        input_layer = tf.keras.layers.Input([input_size, input_size, 3])
+        if FLAGS.tiny:
+            feature_maps = YOLOv3_tiny(input_layer, NUM_CLASS)
+            bbox_tensors = []
+            for i, fm in enumerate(feature_maps):
+                bbox_tensor = decode(fm, NUM_CLASS, i)
+                bbox_tensors.append(bbox_tensor)
+            model = tf.keras.Model(input_layer, bbox_tensors)
+            utils.load_weights_tiny(model, FLAGS.weights)
+        else:
+            if FLAGS.model == 'yolov3':
+                feature_maps = YOLOv3(input_layer, NUM_CLASS)
+                bbox_tensors = []
+                for i, fm in enumerate(feature_maps):
+                    bbox_tensor = decode(fm, NUM_CLASS, i)
+                    bbox_tensors.append(bbox_tensor)
+                model = tf.keras.Model(input_layer, bbox_tensors)
+                utils.load_weights_v3(model, FLAGS.weights)
+            elif FLAGS.model == 'yolov4':
+                feature_maps = YOLOv4(input_layer, NUM_CLASS)
+                bbox_tensors = []
+                for i, fm in enumerate(feature_maps):
+                    bbox_tensor = decode(fm, NUM_CLASS, i)
+                    bbox_tensors.append(bbox_tensor)
+                model = tf.keras.Model(input_layer, bbox_tensors)
+                utils.load_weights(model, FLAGS.weights)
+    else:
+        # Load TFLite model and allocate tensors.
+        interpreter = tf.lite.Interpreter(model_path=FLAGS.weights)
+        interpreter.allocate_tensors()
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+        print(input_details)
+        print(output_details)
+        
+    host = ''        # Symbolic name meaning all available interfaces
+    port = 45678     # Arbitrary non-privileged port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+
+    s.listen(1)
+    print('Initializing connection')
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    data = (conn.recv(1024))
+    data = json.loads(data.decode())
+
+    conn.sendall(json.dumps(data).encode())
+    print('Recived iniatializing data')
+    shape = data.get("shape")
+    shared_memory_name = data.get("name")
+    type_data = data.get("type")
+    s.setblocking(0)
+    
+
+    # Attach to the existing shared memory block
+    existing_shm = shared_memory.SharedMemory(name=shared_memory_name)
+    # Note that a.shape is (6,) and a.dtype is np.int64 in this example
+    previous = None
+    frame = None
+    print('Esperando imágenes')
+    while True:
+        start_time = time.time()
+        print('Waiting frame')
+        data = (conn.recv(1024))
+        if data == bytes('1', 'utf8'):
+            previous = frame
+            frame = np.ndarray(shape, dtype=type_data, buffer=existing_shm.buf)
+            frame = process(frame,input_size,model, FLAGS, ANCHORS, STRIDES,XYSCALE)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            conn.sendall(bytes("1",'utf8'))
+            conn.sendall(bytes("1",'utf8'))
+            print("FPS: ", 1.0 / (time.time() - start_time))
+  
+
+def process(frame,input_size,model, FLAGS, ANCHORS, STRIDES,XYSCALE ):
+   
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray((frame * 255).astype(np.uint8))
+
+        frame_size = frame.shape[:2]
+        image_data = utils.image_preporcess(np.copy(frame), [input_size, input_size])
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+        prev_time = time.time()
+
+        if FLAGS.framework == 'tf':
+            pred_bbox = model.predict(image_data)
+        else:
+            interpreter.set_tensor(input_details[0]['index'], image_data)
+            interpreter.invoke()
+            pred_bbox = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+
+        if FLAGS.model == 'yolov4':
+            pred_bbox = utils.postprocess_bbbox(pred_bbox, ANCHORS, STRIDES, XYSCALE)
+        else:
+            pred_bbox = utils.postprocess_bbbox(pred_bbox, ANCHORS, STRIDES)
+
+        bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.25)
+        bboxes = utils.nms(bboxes, 0.213, method='nms')
+
+        image = utils.draw_bbox(frame, bboxes)
+        curr_time = time.time()
+        exec_time = curr_time - prev_time
+        result = np.asarray(image)
+        info = "time: %.2f ms" %(1000*exec_time)
+        print(info)
+        result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        return result
+if __name__ == "__main__":
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
+
+
+
+
+existing_shm.close()
