71b2359978bc3ce74b42a441774ac27992beb0a3
CristianRod21 <cjrs21@hotmail.com>
2020-06-11 11:35:17 -0600

    Changed custom words to COCO words

--- find_word.py
+++ find_word.py
@@ -29,7 +29,7 @@ def find_word(input_word):
 	print("palabra en inglés: " + english_word)
 	#.decode("utf-8")
 
-	words = ['bottle', 'book', 'keyboard', 'laptop', 'mobile phone', 'mouse', 'pen' ]
+	words = ['person','bicycle','car','motorbike','aeroplane','bus','train','truck','boat','traffic light','fire hydrant','stop sign','parking meter','bench','bird','cat','dog','horse','sheep','cow','elephant','bear','zebra','giraffe','backpack','umbrella','handbag','tie','suitcase','frisbee','skis','snowboard','sports ball','kite','baseball bat','baseball glove','skateboard','surfboard','tennis racket','bottle','wine glass','cup','fork','knife','spoon','bowl','banana','apple','sandwich','orange','broccoli','carrot','hot dog','pizza','donut','cake','chair','sofa','potted plant','bed','dining table','toilet','tvmonitor','laptop','mouse','remote','keyboard','cell phone','microwave','oven','toaster','sink','refrigerator','book','clock','vase','scissors','teddy bear','hair drier','toothbrush']
 
 	is_valid = False
 
